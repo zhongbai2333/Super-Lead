eba85d83064015c84799bd5e3bb3229b3fe4043a
zhongbai233 <54356787+zhongbai2333@users.noreply.github.com>
2026-05-14 02:30:09 +0800

    Update ParrotRopePerchController.java

--- src/main/java/com/zhongbai233/super_lead/lead/ParrotRopePerchController.java
+++ src/main/java/com/zhongbai233/super_lead/lead/ParrotRopePerchController.java
@@ -11,7 +11,6 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.parrot.Parrot;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.entity.EntityTypeTest;
 import net.minecraft.world.phys.AABB;
